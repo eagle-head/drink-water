d600309f7ea6f928ac011a1079434f7367b82fa8
eagle-head <edy.kohn@gmail.com>
2023-02-22 11:34:05 -0300

    Add babel-plugin-module-resolver configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".android.js", ".android.tsx", ".ios.js", ".ios.tsx"],
+        alias: {
+          "@/enum": "./src/enum",
+          "@/test": "./src/test",
+          "@/hooks": "./src/hooks",
+          "@/utils": "./src/utils",
+          "@/routes": "./src/routes",
+          "@/assets": "./src/assets",
+          "@/stores": "./src/stores",
+          "@/context": "./src/context",
+          "@/storage": "./src/storage",
+          "@/features": "./src/features",
+          "@/providers": "./src/providers",
+          "@/components": "./src/components",
+        },
+      },
+    ],
+  ],
 };
