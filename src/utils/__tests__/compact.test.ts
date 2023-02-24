b9d143026bacbbfdf909a880b74bf6887451e36a
eagle-head <edy.kohn@gmail.com>
2023-02-23 21:05:44 -0300

    Delete unnecessary test in compact.test.ts file

--- src/utils/__tests__/compact.test.ts
+++ src/utils/__tests__/compact.test.ts
@@ -6,11 +6,6 @@ describe("compact function", () => {
     expect(compact(input)).toEqual([1, 2, 3]);
   });
 
-  test("should return an empty array when passed null or undefined", () => {
-    expect(compact([null])).toEqual([]);
-    expect(compact([undefined])).toEqual([]);
-  });
-
   test("should not modify the original array", () => {
     compact(input);
     expect(input).toEqual([1, null, 2, undefined, 3]);
