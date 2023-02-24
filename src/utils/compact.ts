03b4c939bcf25dfd5500001a5e4be3178d4db3e3
eagle-head <edy.kohn@gmail.com>
2023-02-23 21:04:42 -0300

    Delete unnecessary IF in compact function

--- src/utils/compact.ts
+++ src/utils/compact.ts
@@ -1,10 +1,6 @@
 export function compact<T>(array: (T | null | undefined)[]): T[] {
   const result: T[] = [];
 
-  if (array == null) {
-    return result;
-  }
-
   for (let i = 0; i < array.length; i++) {
     const value = array[i];
     if (value) {
