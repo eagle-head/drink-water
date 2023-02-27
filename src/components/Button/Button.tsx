b8bdf06e4ec4bfd4bc6ad6d850c1461a80a0bd54
eagle-head <edy.kohn@gmail.com>
2023-02-27 10:14:38 -0300

    Add React.memo to Button component.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,39 +7,41 @@ import { Pressable, Label } from "./button.styles";
 import { useTheme } from "@/contexts";
 import { compact } from "@/utils";
 
-export const Button: RNElement<StyledButton> = ({ size, label, variant, isDisabled, isLoading, isPressed }) => {
-  const { theme } = useTheme();
+export const Button: RNElement<StyledButton> = React.memo(
+  ({ size, label, variant, isDisabled, isLoading, isPressed }) => {
+    const { theme } = useTheme();
 
-  const activityIndicatorColor = (() => {
-    if (variant === "outlined") {
-      return isDisabled || isLoading ? theme.colors.disabled : theme.colors.primary;
-    }
+    const activityIndicatorColor = (() => {
+      if (variant === "outlined") {
+        return isDisabled || isLoading ? theme.colors.disabled : theme.colors.primary;
+      }
 
-    return theme.colors.secondary;
-  })();
+      return theme.colors.secondary;
+    })();
 
-  return (
-    <Pressable
-      android_ripple={{ color: isDisabled || isLoading ? "transparent" : theme.colors.primaryDark }}
-      style={({ pressed }) => compact([pressed ? { opacity: 0.75 } : null])}
-      testOnly_pressed={isPressed}
-      variant={variant}
-      isDisabled={isDisabled}
-      disabled={isDisabled || isLoading}
-      isLoading={isLoading}
-      size={size}
-      accessibilityRole="button">
-      {isLoading ? (
-        <ActivityIndicator
-          size={size === "large" ? "large" : "small"}
-          color={activityIndicatorColor}
-          testID="button-activity-indicator"
-        />
-      ) : (
-        <Label size={size} variant={variant} isDisabled={isDisabled} isLoading={isLoading} accessibilityRole="text">
-          {label}
-        </Label>
-      )}
-    </Pressable>
-  );
-};
+    return (
+      <Pressable
+        android_ripple={{ color: isDisabled || isLoading ? "transparent" : theme.colors.primaryDark }}
+        style={({ pressed }) => compact([pressed ? { opacity: 0.75 } : null])}
+        testOnly_pressed={isPressed}
+        variant={variant}
+        isDisabled={isDisabled}
+        disabled={isDisabled || isLoading}
+        isLoading={isLoading}
+        size={size}
+        accessibilityRole="button">
+        {isLoading ? (
+          <ActivityIndicator
+            size={size === "large" ? "large" : "small"}
+            color={activityIndicatorColor}
+            testID="button-activity-indicator"
+          />
+        ) : (
+          <Label size={size} variant={variant} isDisabled={isDisabled} isLoading={isLoading} accessibilityRole="text">
+            {label}
+          </Label>
+        )}
+      </Pressable>
+    );
+  }
+);
