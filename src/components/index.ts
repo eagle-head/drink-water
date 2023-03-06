17672f9f8aa6edd5d9098e53225a80d3f3f0ca86
eagle-head <edy.kohn@gmail.com>
2023-03-06 20:29:12 -0300

    Remove custom-circular-progress-bar.

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@
 export * from "./Button/Button";
 export * from "./Typography/Typography";
 export * from "./Divider/Divider";
-export * from "./CircularProgressBar/CircularProgressBar";
 export * from "./IconButton/IconButton";
 export * from "./Input/Input";
 export * from "./Toast/Toast";
